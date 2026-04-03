b83bb54d14c4776e0239e6bddf86d0428049a7a8
Selin L. Erickson <Selinlily@gmail.com>
2026-04-03 17:24:13 -0500

    Add files via upload

---
+++ code/Q1_code.py
@@ -0,0 +1,44 @@
+'''
+Homework [5]: FFT- PDF Solving
+    S. Lily Erickson
+    Spring 2026
+This code solves Question 1 parts a, b, and c. 
+'''
+from tkinter.messagebox import QUESTION
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# ================================== 
+# QUESTION 1 SETUP
+# ==================================
+
+
+
+
+
+
+
+
+# ================================== 
+# PART (A) 
+# ==================================
+
+
+
+
+
+
+
+# ================================== 
+# PART (B) 
+# ==================================
+
+
+
+
+
+# ================================== 
+# PART (C) 
+# ==================================
+
---
+++ code/Q2_code.py
@@ -0,0 +1,36 @@
+
+'''
+Homework [5]: FFT- PDF Solving
+    S. Lily Erickson
+    Spring 2026
+This code solves Question 2 parts a and b.
+'''
+from tkinter.messagebox import QUESTION
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# ================================== 
+# QUESTION 2 SETUP
+# ==================================
+
+
+
+
+
+
+
+
+# ================================== 
+# PART (A) 
+# ==================================
+
+
+
+
+
+
+
+# ================================== 
+# PART (B) 
+# ==================================
